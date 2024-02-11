5b46f55e3cd6518b555a519d96f6a02cc02a2e53
shritipali <c001a01qb@gmail.com>
2024-02-11 21:58:45 +0530

    rage

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './App.css';
+// import Connect from './components/Wallet/Connect';
+import Cur from './components/Cur';
+import Connect from './components/Wallet/Connect';
+
+const App = () => {
+	
+	
+
+	return (
+		<>
+		<Cur/>
+		<Connect/>
+		</>
+		
+	);
+};
+
+export default App;
